3333af97bfac0a12f309596e19232613b8e7e5aa
broeker <tim@electriccitizen.com>
2021-01-01 04:46:05 -0600

    postcss fix

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,5 @@ module.exports = {
   plugins: [
     require(`tailwindcss`)(`./styles/tailwind.config.js`),
     require(`autoprefixer`),
-    ...(process.env.NODE_ENV === "production"
-      ? [require(`clean-css`)]
-      : []),
   ],
 };
