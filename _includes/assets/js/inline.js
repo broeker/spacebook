ba5642d9553c8c748838015cf453b5c7683cb057
Grant Holle <hollegrant@gmail.com>
2021-07-24 10:33:19 +0800

    Fix #26 - improve dark mode toggle
    I've added the dark mode on be default if that's your os setting. I also moved the functionality outside of the `defer` block, as there was flickering in the top header area when on dark mode.

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -9,15 +9,10 @@ if (window.netlifyIdentity) {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  
+
   const el = document.getElementById("main");
   el.addEventListener("click", closeNavigation, false)
 
-  if (localStorage.getItem('darkmode') === 'true') {
-    document.body.classList.add("dark");
-  } else {
-    document.body.classList.remove("dark");
-  }
 });
 
 function logout() {
@@ -27,24 +22,14 @@ function logout() {
 
 function showNavigation() {
   const navigation = document.getElementById("navigation");
-  navigation.classList.remove("hidden", "sticky","pt-32"); 
-  navigation.classList.add("absolute","right-0", "top-0", "-mt-0", "z-50", "pt-0", "bg-white","border-l", "border-gray-200"); 
+  navigation.classList.remove("hidden", "sticky","pt-32");
+  navigation.classList.add("absolute","right-0", "top-0", "-mt-0", "z-50", "pt-0", "bg-white","border-l", "border-gray-200");
 }
 
 function closeNavigation() {
   const navigation = document.getElementById("navigation");
-  navigation.classList.add("hidden"); 
-  navigation.classList.remove("absolute","right-0","z-50", "bg-gray-100", "border-r", "border-gray-800" ); 
-}
-
-function activateDarkMode() {
-  if (localStorage.getItem('darkmode') === 'true') {
-    localStorage.setItem('darkmode', 'false')
-    document.body.classList.remove("dark");
-  } else {
-    localStorage.setItem('darkmode', 'true')
-    document.body.classList.add("dark");
-  }
+  navigation.classList.add("hidden");
+  navigation.classList.remove("absolute","right-0","z-50", "bg-gray-100", "border-r", "border-gray-800" );
 }
 
 function toggleLayout(state) {
@@ -58,8 +43,7 @@ function toggleLayout(state) {
     } else {
       localStorage.setItem('layout', 'horizontal')
     }
-  } 
-  
- 
+  }
+
   console.log(localStorage.getItem('layout'))
 }
